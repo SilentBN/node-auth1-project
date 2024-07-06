055488d3ff4958810518e1d56489b287d8f5405f
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-07-05 20:58:50 -0400

    Implement authentication and user endpoints, set up session middleware

--- api/server.js
+++ api/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -21,11 +25,29 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use(
+  session({
+    name: "chocolatechip",
+    secret: "keep it secret, keep it safe!",
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
